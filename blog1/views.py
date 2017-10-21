18d44849b8d8db34d08678134fde365d1027dfc0
mdondeti <vdondeti@unomaha.edu>
2017-10-20 19:14:29 -0500

    AI integrated blog running on Local Machine.

--- blog1/views.py
+++ blog1/views.py
@@ -4,9 +4,36 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+import json
+from watson_developer_cloud import ToneAnalyzerV3
+from watson_developer_cloud import LanguageTranslatorV2 as LanguageTranslator
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    tone_analyzer = ToneAnalyzerV3(
+        username='f86d18cc-a579-4b13-8ea6-1a80dd039208',
+        password='rROy4mUncQDa',
+        version='2016-05-19 ')
+
+    language_translator = LanguageTranslator(
+        username='e68faa60-6e71-42e0-a5bc-a98e170c2826',
+        password='DG6R6N3PNAnK')
+
+
+    for post in posts:
+        data = json.dumps(tone_analyzer.tone(text=post.text), indent=1)
+        j = json.loads(data);
+        post.info = j['document_tone']['tone_categories'][0]['tones']
+        post.angerScore = post.info[0]['score']
+        post.disgustScore = post.info[1]['score']
+        post.fearScore = post.info[2]['score']
+        post.joyScore = post.info[3]['score']
+        post.sadScore = post.info[4]['score']
+    translation = language_translator.translate(
+        text=post.text,
+        source='en',
+        target='es')
+    post.translatedText = json.dumps(translation, indent=2, ensure_ascii=False)
     return render(request, 'blog1/post_list.html', {'posts': posts})
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
